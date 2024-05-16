ab826dc01687c1034de2f84671480ce904896b26
gabrielpaixao17 <161714054+gabrielpaixao17@users.noreply.github.com>
2024-05-16 08:40:44 -0300

    Add files via upload

---
+++ gabriel p/lista.java
@@ -0,0 +1,53 @@
+import java.util.Arraylist;
+import java.util.lista;
+
+public class ColecoesBasicas {
+    public static void main (String [] args){
+        List<String> Alunos  = new  Arraylist<>{}
+        {{
+            add(" Gabriel ");
+            add(" Amanda ");
+            add(" Ruan ");
+            add(" Fernanda");
+            add(" Santize ");
+        }};
+
+        System.out.println("A lista dos alunos:" + Alunos: ); 
+          {{
+            add(" Matrícula I ");
+            add(" Matrícula II ");
+            add(" Matrícula III ");
+            add(" Matrícula IV ");
+            add(" Matrícula V ");
+
+          }};
+
+          System.out.println(" Cadastro por Matrícula :" );
+
+          public void setLancamento(Date data){
+            this.lancamento = data;
+
+              {{
+                add(" 11/11/2011 ");
+                add(" 12/12/2012 ");
+                add(" 01/01/2001 ");
+                add(" 05/05/2005 ");
+                add(" 07/07/2007 ");
+              }};
+
+              System.out.println(" A data de Nascimento dos alunos, é :");
+          }
+
+          import java.util.Scanner;
+
+          public class elementos{
+            public static void main ( )
+          }
+
+          }
+
+          
+
+
+
+    }
